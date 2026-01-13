28486041387b5046f8e6b15d4a468f23ad9e1799
INDRA KUMAR BEJJIPURAM <tm88indrakumar2020@gmail.com>
2026-01-13 16:24:30 +0530

    files uploaded

---
+++ Day 14/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAP2025.Day_14
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public Employee()
+        {
+        }
+
+    }
+    public class ProgramMain
+    {
+        public static void Main(string[] args)
+        {
+            Employee emp = new Employee
+            {
+                Id=12204083,
+                Name="Indra",
+                Salary=1500000,
+            };
+            Console.WriteLine(emp.Name);
+        }
+    }
+}
---
+++ Day 15/EmployeeGeneric.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CAP2025.Day_15
+{
+    public class EmployeeRe
+    {
+        public string Name { get; set; }
+        public int EmployeeID { get; set; }
+        public int Rank { get; set; }
+        public class ExampleWorkOutGeneric
+        {
+            public static List<EmployeeRe> EmployeeReList = new List<EmployeeRe>();
+            public ExampleWorkOutGeneric()
+            {
+
+            }
+            public static void Main(string[] args)
+            {
+                var localEmpList = GetData();
+                var filterList = from emp in localEmpList select emp;
+                foreach(EmployeeRe emp in filterList)
+                {
+                    emp.Name= emp.Name;
+                }
+            }
+            public List<EmployeeRe> GetData()
+            {
+                EmployeeReList.Add(new EmployeeRe() { EmployeeID = 1, Name = "A", Rank = 1 });
+                EmployeeReList.Add(new EmployeeRe() { EmployeeID = 2, Name = "B", Rank = 2 });
+                EmployeeReList.Add(new EmployeeRe() { EmployeeID = 3, Name = "C", Rank = 4 });
+                return EmployeeReList;
+            }
+        }
+
+    }
+}
